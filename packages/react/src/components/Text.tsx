71b0c15a495903721b41d8c186f022025df36021
Alexandre Kaimer <alekaimer@gmail.com>
2023-05-30 19:02:03 -0300

    Change colors tokens and adds className as prop in Text component (#8)
    * feat: Enable workflow to CI/CD and publish to NPM
    
    * refactor: Change colors tokens
    
    * refactor: Add className as prop in Text component
    
    * docs(TextComponent): Up version

--- packages/react/src/components/Text.tsx
+++ packages/react/src/components/Text.tsx
@@ -1,15 +1,16 @@
 import React, { ComponentProps, ReactNode } from 'react'
+import { clsx } from 'clsx'
 
 export interface TextProps extends ComponentProps<'p'> {
   children: ReactNode
   tagName?: 'p' | 'span'
 }
 
-export function Text({ children, tagName, ...props }: TextProps) {
+export function Text({ children, tagName, className, ...props }: TextProps) {
   const Component = (tagName as React.ElementType) || 'p'
 
   return (
-    <Component className="text-gray-400" {...props}>
+    <Component className={clsx('text-gray-400', className)} {...props}>
       {children}
     </Component>
   )
