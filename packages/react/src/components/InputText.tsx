7579b00a6b73446dd15bdac574d34bbf25bbc462
Alexandre Kaimer <alekaimer@gmail.com>
2023-06-06 22:29:57 -0300

    Add className as prop to InputText component (#18)
    * refactor: Add className as prop to InputText component

--- packages/react/src/components/InputText.tsx
+++ packages/react/src/components/InputText.tsx
@@ -1,19 +1,23 @@
 import { ComponentProps, forwardRef, ElementRef } from 'react'
+import { clsx } from 'clsx'
 
 export interface InputTextProps extends ComponentProps<'input'> {
   prefix?: string
 }
 
 export const InputText = forwardRef<ElementRef<'input'>, InputTextProps>(
-  ({ prefix, ...props }: InputTextProps, ref) => {
+  ({ prefix, className, ...props }: InputTextProps, ref) => {
     return (
       <div
-        className="
+        className={clsx(
+          `
           flex items-center border border-gray-300 rounded-md transition-all
           px-3 py-2
           hover:border-info-default
           focus-within:ring-1 focus-within:ring-info-default focus-within:border-info-default
-        "
+          `,
+          className,
+        )}
       >
         {!!prefix && <span className="text-gray-400">{prefix}</span>}
         <input
